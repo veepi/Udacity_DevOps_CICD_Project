11c80ebba00226965d47abbe7c72e65ef1bc6742
vidura perera <vidura.perera@gmail.com>
2021-07-31 10:59:50 +0000

    app updates from AZ CS

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def predict():
     
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction, 'app_version':'1.0'})
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
